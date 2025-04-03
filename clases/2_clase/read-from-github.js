c0fbc28dd9b74167ace62e037fa9461d4c144ee6
Andru-1987 <anderson.ocana@bue.edu.ar>
2025-04-03 06:40:51 -0300

    [ADD] Material de clases-> aprox clase 13

--- clases/2_clase/read-from-github.js
+++ clases/2_clase/read-from-github.js
@@ -3,7 +3,5 @@ async function readDataFromGitHub(uriFilePath) {
     console.log(response)
 }
 
-
-
 const URI = "https://raw.githubusercontent.com/ropensci/historydata/refs/heads/master/data-raw/quasi-war.csv"
 readDataFromGitHub(URI)
